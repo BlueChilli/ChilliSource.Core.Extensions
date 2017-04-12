2e9d441a74dfb1c016b4d34e7b59734c239818da
onikiri2007 <onikiri2007@gmail.com>
2017-04-13 07:23:39 +1000

    fix build script to use msbuild

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -8,6 +8,6 @@ using System.Reflection;
 [assembly: AssemblyProduct("ChilliSource Core Extensions")]
 [assembly: AssemblyVersion("1.0.1")]
 [assembly: AssemblyFileVersion("1.0.1")]
-[assembly: AssemblyInformationalVersion("1.0.1-alpha.7+Branch.develop.Sha.24ad93e5e3bbfbd57e4cf81e6a82579ce6c3d431")]
+[assembly: AssemblyInformationalVersion("1.0.1-alpha.9+Branch.develop.Sha.40205971f59ffeb16c72180e95cc483fddc46a1a")]
 [assembly: AssemblyCopyright("Copyright Blue Chilli Technology Pty Ltd 2017")]
 
