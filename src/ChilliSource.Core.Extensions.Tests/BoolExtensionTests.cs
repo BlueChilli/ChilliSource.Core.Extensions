32d8cb1ac39a93bcacb17d275ddbdd6e58241a6d
Ella <Ella@bluechilli.com>
2017-04-18 13:36:42 +1000

    working on Unit tests on Boolean and Collections extensions

---
+++ src/ChilliSource.Core.Extensions.Tests/BoolExtensionTests.cs
@@ -0,0 +1,44 @@
+﻿#region License
+
+/*
+Licensed to Blue Chilli Technology Pty Ltd and the contributors under the MIT License (the "License").
+You may not use this file except in compliance with the License.
+See the LICENSE file in the project root for more information.
+*/
+
+#endregion
+
+using System;
+using ChilliSource.Core.Extensions;
+using Xunit;
+
+namespace Tests
+{
+	public class BoolExtensionTests
+    {
+
+		[Fact]
+		public void ToInt_ShouldReturnEquivalentInteger_WhenGivenBoolean()
+		{
+            bool input = true;
+			var result = input.ToInt();
+			Assert.Equal(1, result);
+
+            input = false;
+            result = input.ToInt();
+            Assert.Equal(0, result);
+        }
+
+		[Fact]
+		public void Toggle_ShouldNegateValue_WhenGivenBoolean()
+		{
+            bool input = true;
+            var result = input.Toggle();
+            Assert.Equal(false, result);
+
+            input = false;
+            result = input.Toggle();
+            Assert.Equal(true, result);
+        }
+	}
+}
---
+++ src/ChilliSource.Core.Extensions.Tests/CollectionExtensionsTests.cs
@@ -0,0 +1,30 @@
+﻿#region License
+
+/*
+Licensed to Blue Chilli Technology Pty Ltd and the contributors under the MIT License (the "License").
+You may not use this file except in compliance with the License.
+See the LICENSE file in the project root for more information.
+*/
+
+#endregion
+
+using System;
+using ChilliSource.Core.Extensions;
+using Xunit;
+using System.Collections.Generic;
+
+namespace Tests
+{
+	public class CollectionExtensionsTests
+    {
+
+		[Fact]
+		public void AddOrUpdate_ShouldAddItem_WhenDoesntExist()
+		{
+            var input = new List<int> { 1, 2 };
+            input.AddOrUpdate(3, (Entity) => Entity);
+            Assert.Contains(3, input);
+        }
+
+    }
+}
