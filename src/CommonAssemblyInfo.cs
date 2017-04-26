bb6c04bdf1b8af2be98c5556db35eff71f697f06
Will <will@bluechilli.com>
2017-04-26 16:06:02 +1000

    build changes

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -8,6 +8,6 @@ using System.Reflection;
 [assembly: AssemblyProduct("ChilliSource Core Extensions")]
 [assembly: AssemblyVersion("1.0.1")]
 [assembly: AssemblyFileVersion("1.0.1")]
-[assembly: AssemblyInformationalVersion("1.0.1-alpha.10+Branch.develop.Sha.2e9d441a74dfb1c016b4d34e7b59734c239818da")]
+[assembly: AssemblyInformationalVersion("1.0.1-alpha.20+Branch.develop.Sha.3c924723472f719fd180ab0b395d60c24a00f1ce")]
 [assembly: AssemblyCopyright("Copyright Blue Chilli Technology Pty Ltd 2017")]
 
