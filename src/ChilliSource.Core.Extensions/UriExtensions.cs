f69edff03b67cacf14342187dc2d5ec89802e4b2
david <drgrieve@hotmail.com>
2017-05-03 09:52:13 +1000

    uri extensions

---
+++ src/ChilliSource.Core.Extensions/UriExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChilliSource.Core.Extensions
+{ 
+    /// <summary>
+    /// Extension methods for System.Uri.
+    /// </summary>
+    public static class UriExtensions
+    {
+        /// <summary>
+        /// Gets the host component of the specified System.Uri, without "www.".
+        /// </summary>
+        /// <param name="uri">A System.Uri.</param>
+        /// <returns>The host component of the URI.</returns>
+        public static string AsFriendlyName(this Uri uri)
+        {
+            return uri.Host.TrimStart("www.");
+        }
+
+        /// <summary>
+        /// Gets the root of the specified System.Uri (everything except the path and the query string)
+        /// </summary>
+        /// <param name="uri">A System.Uri.</param>
+        /// <returns>The root component of the URI.</returns>
+        public static string Root(this Uri uri)
+        {
+            return uri.GetComponents(UriComponents.Scheme | UriComponents.Host | UriComponents.Port, UriFormat.UriEscaped);
+        }
+
+        /// <summary>
+        /// Gets the base of the specified System.Uri (everything except the query string)
+        /// </summary>
+        /// <param name="uri">A System.Uri.</param>
+        /// <returns>The base component of the URI.</returns>
+        public static string Base(this Uri uri)
+        {
+            return uri.GetComponents(UriComponents.Scheme | UriComponents.Host | UriComponents.Port | UriComponents.Path, UriFormat.UriEscaped);
+        }
+    }
+}
