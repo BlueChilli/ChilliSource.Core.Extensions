eb0f9d525dd4282cb6ddab61145c8a2d3db41284
drgrieve543 <drgrieve543@gmail.com>
2021-10-18 18:09:28 +1100

    move dic extensions from mvc to here

---
+++ src/ChilliSource.Core.Extensions.Tests/DictionaryExtensionTests.cs
@@ -0,0 +1,38 @@
+﻿#region License
+
+/*
+Licensed to Blue Chilli Technology Pty Ltd and the contributors under the MIT License (the "License").
+You may not use this file except in compliance with the License.
+See the LICENSE file in the project root for more information.
+*/
+
+#endregion
+
+using System;
+using ChilliSource.Core.Extensions;
+using Xunit;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Tests
+{
+	public class DictionaryExtensionTests
+    {
+
+		[Fact]
+		public void HasValueWorks()
+		{
+            var test = new Dictionary<int, string> { { 1, "Jim" }, { 2, "Jane" }, { 3, "Fred" } };
+
+            Assert.True(test.HasValue(1, "Jim"));
+            Assert.True(test.HasValue(2, "Jane"));
+            Assert.True(test.HasValue(3, "Fred"));
+
+            Assert.False(test.HasValue(2, "Jim"));
+            Assert.False(test.HasValue(1, "Jane"));
+            Assert.False(test.HasValue(int.MinValue, null));
+            Assert.False(test.HasValue(1, null));
+        }
+
+    }
+}
