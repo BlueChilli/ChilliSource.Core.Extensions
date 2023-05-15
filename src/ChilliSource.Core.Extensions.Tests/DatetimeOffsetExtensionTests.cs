2fb9f1d1d73ce3be6aba1d1d893bf62814ce0990
drgrieve543 <drgrieve543@gmail.com>
2023-05-15 16:42:23 +1000

    addworkingdays for datetimeoffset

---
+++ src/ChilliSource.Core.Extensions.Tests/DatetimeOffsetExtensionTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ChilliSource.Core.Extensions.Tests
+{
+    public class DatetimeOffsetExtensionTests
+    {
+        [Fact]
+        public void TestAddWorkingDays()
+        {
+            var testDate1 = new DateTimeOffset(2016, 8, 1, 0, 0, 0, new TimeSpan(10, 0, 0)); //Sydney
+
+            var date1 = testDate1.AddWorkingDays(3);
+            Assert.True(date1.DateTime == new DateTime(2016, 8, 4));
+
+            var date2 = testDate1.AddWorkingDays(0);
+            Assert.True(date2.DateTime == new DateTime(2016, 8, 1));
+
+            var date3 = testDate1.AddWorkingDays(-3);
+            Assert.True(date3.DateTime == new DateTime(2016, 7, 27));
+
+
+            var testDate2 = new DateTimeOffset(2016, 8, 13, 0, 0, 0, new TimeSpan(12, 0, 0)); //NZ
+
+            date1 = testDate2.AddWorkingDays(5);
+            Assert.True(date1.DateTime == new DateTime(2016, 8, 19));
+
+            date2 = testDate2.AddWorkingDays(0);
+            Assert.True(date2.DateTime == new DateTime(2016, 8, 15));
+
+            date3 = testDate2.AddWorkingDays(-5);
+            Assert.True(date3.DateTime == new DateTime(2016, 8, 8));
+
+            var testDate3 = new DateTimeOffset(2016, 8, 15, 0, 0, 0, new TimeSpan(8, 0, 0)); //Perth
+
+            date1 = testDate3.AddWorkingDays(5);
+            Assert.True(date1.DateTime == new DateTime(2016, 8, 22));
+
+            date1 = new DateTime(2016, 8, 19).AddWorkingDays(5);
+            Assert.True(date1.DateTime == new DateTime(2016, 8, 26));
+        }
+    }
+}
---
+++ src/ChilliSource.Core.Extensions/DateTimeOffsetExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChilliSource.Core.Extensions
+{
+
+    /// <summary>
+    /// DatetimeOffset extensions.
+    /// </summary>
+    public static class DateTimeOffsetExtensions
+    {
+        /// <summary>
+        /// Gets a System.DateTimeOffset object by adding number of working days to the specified date. (Working days exclude Saturday, Sunday, but include all holidays).
+        /// </summary>
+        /// <param name="date">The specified date.</param>
+        /// <param name="workingDays">Number of working days.</param>
+        /// <returns>A System.DateTime object.</returns>
+        public static DateTimeOffset AddWorkingDays(this DateTimeOffset date, int workingDays)
+        {
+            int direction = workingDays < 0 ? -1 : 1;
+            var newDate = date;
+            while (workingDays != 0 || !newDate.DateTime.IsWorkingDay())
+            {
+                newDate = newDate.AddDays(direction);
+                while (!newDate.DateTime.IsWorkingDay())
+                {
+                    newDate = newDate.AddDays(direction);
+                };
+                if (workingDays != 0)
+                {
+                    workingDays -= direction;
+                }
+            }
+            return newDate;
+        }
+    }
+}
