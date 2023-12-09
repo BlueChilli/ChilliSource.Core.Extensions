dbc2260f569ef2abb200f18e887c3c573425b82b
drgrieve543 <drgrieve543@gmail.com>
2023-12-09 14:19:46 +1100

    move stream extensions to here

--- src/ChilliSource.Core.Extensions/StreamExtensions.cs
+++ src/ChilliSource.Core.Extensions/StreamExtensions.cs
@@ -10,6 +10,9 @@ See the LICENSE file in the project root for more information.
 
 using System;
 using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace ChilliSource.Core.Extensions
@@ -65,6 +68,57 @@ namespace ChilliSource.Core.Extensions
                 await stream.CopyToAsync(ms2);
                 return ms2.ToArray();
             }
+        }
+
+        public static T DeserializeTo<T>(this Stream stream, StreamSerializationOptions options = null)
+        {
+            options = options ?? new StreamSerializationOptions();
+
+            try
+            {
+                if (stream == null)
+                    return default(T);
+
+                if (options.SkipFormatterForStrings && typeof(T) == typeof(string))
+                {
+                    using (var streamReader = new StreamReader(stream, Encoding.UTF8, false, 4096, leaveOpen: true))
+                    {
+                        object value = streamReader.ReadToEnd();
+                        return (T)value;
+                    }
+                }
+
+                var formatter = options.FormatterFactory();
+                var obj = formatter.Deserialize(stream);
+                return (T)obj;
+            }
+            finally
+            {
+                if (!options.LeaveOpen && stream != null)
+                {
+                    stream.Dispose();
+                }
+            }
+        }
+
+        public class StreamSerializationOptions
+        {
+            public StreamSerializationOptions()
+            {
+                this.LeaveOpen = false;
+                this.FormatterFactory = DefaultFormatter;
+            }
+
+            private static IFormatter DefaultFormatter()
+            {
+                return new BinaryFormatter();
+            }
+
+            public bool LeaveOpen { get; set; }
+
+            public bool SkipFormatterForStrings { get; set; }
+
+            public Func<IFormatter> FormatterFactory { get; set; }
         }
     }
 }
